73518b74e78991d8f4b56aa87395bcad28a624bb
Renji Kashii <32693797+RenjiKashii@users.noreply.github.com>
2022-07-11 21:12:25 +0900

    Change Dockerfile and docker-compose.yml to create mysql server image.

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,10 @@ FROM python:3.8-slim-buster
 ENV PYTHONUNBUFFERED=1
 WORKDIR /django
 COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
+RUN apt-get update && \
+    apt-get install -y default-libmysqlclient-dev build-essential && \
+    rm -rf /var/lib/apt/lists/*
+RUN pip3 install --upgrade pip && \
+    pip3 install -r requirements.txt
+RUN mkdir -p /var/log/mysql
+RUN touch /var/log/mysql/mysqld.log
--- core/settings.py
+++ core/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app',
+        'USER': 'user',
+        'PASSWORD': 'p@ssw0rd',
+        'HOST': 'mysql',
+        'PORT': '3306'
     }
 }
 
@@ -105,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
