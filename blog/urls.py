ee5560e65ca407466151d46c321173cb5aa27f67
Renji Kashii <32693797+RenjiKashii@users.noreply.github.com>
2022-07-11 22:48:25 +0900

    Add edit and delete templates for blog.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.Index.as_view(), name="index"),
     path('detail/<pk>/', views.Detail.as_view(), name="detail"),
     path('create/', views.Create.as_view(), name="create"),
+    path('update/<pk>', views.Update.as_view(), name="update"),
+    path('delete/<pk>', views.Delete.as_view(), name="delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Post
 
 
@@ -13,3 +15,13 @@ class Create(CreateView):
     model = Post
     
     fields = ["title", "body", "category", "tags"]
+
+class Update(UpdateView):
+    model = Post
+    fields = ["title", "body", "category", "tags"]
+    
+class Delete(DeleteView):
+    model = Post
+    
+    # 削除したあとに移動する先（トップページ）
+    success_url = "/blog"
