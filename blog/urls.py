6716682ea92a90c698e38a38d95e36fa1aca4d4d
Renji Kashii <32693797+RenjiKashii@users.noreply.github.com>
2022-07-11 22:40:25 +0900

    Add templates for blog.

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.Index.as_view(), name="index"),
+    path('detail/<pk>/', views.Detail.as_view(), name="detail"),
+    path('create/', views.Create.as_view(), name="create"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from .models import Post
 
-# Create your views here.
+
+class Index(ListView):
+    model = Post
+
+class Detail(DetailView):
+    model = Post
+    
+class Create(CreateView):
+    model = Post
+    
+    fields = ["title", "body", "category", "tags"]
